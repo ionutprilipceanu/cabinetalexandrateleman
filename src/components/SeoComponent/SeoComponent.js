ded1f9e8f5b1ca61dfb980d68a2abf1da7b8f705
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-11-23 09:44:15 +0200

    up

--- src/components/SeoComponent/SeoComponent.js
+++ src/components/SeoComponent/SeoComponent.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-// import favicon from '..//..//..//..//static/images/favicon.png'
 import favicon from '..//..//..//static/images/favicon.png'
 
 
