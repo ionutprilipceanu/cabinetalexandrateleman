d15b65caeec85afb7f2d82fb69942b7227ca88d1
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-11-24 08:52:03 +0200

    up

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Intro from "..//components/Intro/Intro"
 import Servicii from "..//components/Servicii/Servicii"
 import Review from "..//components/Review/Review"
 import FooterBottom from "..//components/FooterBottom/FooterBottom"
-import imageCover from "..//..//static/images/img-cover.jpg"
+// import imageCover from "..//..//static/images/img-cover.jpg"
 
 
 export default class index extends Component {
@@ -16,43 +16,42 @@ export default class index extends Component {
     return (
       <div>
 
-          <Helmet>
+<Helmet>
             {/* <!-- Primary Meta Tags --> */}
-            <title>Andreea Teleman - Psihoterapeut | Iași</title>
-            <meta name="title" content="Andreea Teleman - Psihoterapeut | Iași"/>
-            <meta name="description" content="Alexandra mă numesc și sunt psihoterapeut, cabinetul este deschis pentru ședințe personalizate de psihoterapie și consiliere pentru adulți. Învinge depresia și anxietatea."/>
+            <title>Marian Mihalache - Handcrafted leather | Iași  </title>
+            <meta name="title" content="Marian Mihalache - Handcrafted leather | Iași  "/>
+            <meta name="description" content="My name is Marian Mihalache and I love to do stuff from handcrafted leather. Shop handcrafted wallets, bags, watch straps, accessories, gifts and more."/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-            <meta name="keywords" content="psiholog iasi, psihoterapeut iasi, psiholog, depresie, anxietate, tristețe, dezvoltare personală, psihoterapie, anxietate simptome, terapie de familie, terapie de cuplu, terapie online, terapie individuala"></meta>
-            <meta name="author" content="Andreea Teleman"/>
+            <meta name="keywords" content="wallets from leather, bags, watch straps, trouser straps, dog collars, harnesses, agenda / book covers, keychains, bracelets, handmade crafts, portofele din piele, genti din piele, curele pentru ceasuri, zgarda pentru caine, coperti din piele, serviete din piele, strapuri aparate foto,"></meta>
+            <meta name="author" content="Marian Mihalache"/>
             <meta property="og:locale" content="ro_RO"/>
             <meta property="og:type" content="website"/>
-            <meta property="og:title" content="Andreea Teleman - Psihoterapeut | Iași"/>
-            <meta property="og:site_name" content="Andreea Teleman - Psihoterapeut | Iași"></meta>
-            <meta property="og:image" content={imageCover}/>
+            <meta property="og:title" content="Marian Mihalache - Handcrafted leather | Iași"/>
+            <meta property="og:site_name" content="Marian Mihalache - Handcrafted leather | Iași"></meta>
+            <meta property="og:image" content="/images/cover-img.jpg"/>
             <meta property="og:image:width" content="1200"/>
             <meta property="og:image:height" content="630"/>
             <link rel="icon" href={favicon} />
-            <meta property="og:image" content={imageCover}/>
+            <meta property="og:image" content="/images/cover-img.jpg"/>
 	          <meta property="og:image:width" content="1200"/>
 	          <meta property="og:image:height" content="630"></meta>
 
 
             {/* <!-- Open Graph / Facebook --> */}
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://alexandra-teleman.netlify.app/"/>
-            <meta property="og:type" content="website"/>
-            <meta property="og:title" content="Andreea Teleman - Psihoterapeut | Iași"/>
-            <meta property="og:description" content="Alexandra mă numesc și sunt psihoterapeut, cabinetul este deschis pentru ședințe personalizate de psihoterapie și consiliere pentru adulți. Învinge depresia și anxietatea."/>
-            <meta property="og:image" content={imageCover}/>
+            <meta property="og:url" content="https://metatags.io/"/>
+            <meta property="og:title" content="Marian Mihalache - Handcrafted leather | Iași  "/>
+            <meta property="og:description" content="My name is Marian Mihalache and I love to do stuff from handcrafted leather. Shop handcrafted wallets, bags, watch straps, accessories, gifts and more."/>
+            <meta property="og:image" content="/images/cover-img.jpg"/>
 	          <meta property="og:image:width" content="1200"/>
 	          <meta property="og:image:height" content="630"></meta>
 
             {/* <!-- Twitter --> */}
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content="https://metatags.io/"/>
-            <meta property="twitter:title" content="Andreea Teleman - Psihoterapeut | Iași"/>
-            <meta property="twitter:description" content="Alexandra mă numesc și sunt psihoterapeut, cabinetul este deschis pentru ședințe personalizate de psihoterapie și consiliere pentru adulți. Învinge depresia și anxietatea."/>
-            <meta property="og:image" content={imageCover}/>
+            <meta property="twitter:title" content="Marian Mihalache - Handcrafted leather | Iași  "/>
+            <meta property="twitter:description" content="My name is Marian Mihalache and I love to do stuff from handcrafted leather. Shop handcrafted wallets, bags, watch straps, accessories, gifts and more."/>
+            <meta property="twitter:image" content="/images/cover-img.jpg"/>
           </Helmet>
         {/* <SeoComponent/> */}
         <NavBar />
