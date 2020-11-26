9d8a0db5c3c056b74635612c0597b7009f55f422
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-11-26 18:23:18 +0200

    up

--- src/pages/articole/ascultarea.js
+++ src/pages/articole/ascultarea.js
@@ -28,7 +28,7 @@ class Ascultarea extends Component {
             <meta property="og:image:width" content="1200"/>
             <meta property="og:image:height" content="630"/>
             <link rel="icon" href={favicon} />
-            <meta property="og:image" content="..//..//..//static/images/articole/neascultareMeta.jpg"/>
+            <meta property="og:image" content="https://alexandra-teleman.netlify.app/static/neascultare-4bc0770840d274c149da28be3e0c20cb.jpg"/>
 	          <meta property="og:image:width" content="1200"/>
 	          <meta property="og:image:height" content="630"></meta> 
 
@@ -39,7 +39,7 @@ class Ascultarea extends Component {
             <meta property="og:type" content="website"/>
             <meta property="og:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
             <meta property="og:description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
-            <meta property="og:image" content={neascultare}/>
+            <meta property="og:image" content="https://alexandra-teleman.netlify.app/static/neascultare-4bc0770840d274c149da28be3e0c20cb.jpg"/>
 	          <meta property="og:image:width" content="1200"/>
 	          <meta property="og:image:height" content="630"></meta>
 
@@ -48,7 +48,7 @@ class Ascultarea extends Component {
             <meta property="twitter:url" content="https://alexandra-teleman.netlify.app/"/>
             <meta property="twitter:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
             <meta property="twitter:description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
-            <meta property="og:image" content={neascultare}/>
+            <meta property="og:image" content="https://alexandra-teleman.netlify.app/static/neascultare-4bc0770840d274c149da28be3e0c20cb.jpg"/>
           </Helmet>
         <NavBar />
         <article>
