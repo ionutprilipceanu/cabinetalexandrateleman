8b56542be4cb08c741fcc27df39b09ae75b05920
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-11-26 11:56:02 +0200

    up

--- src/pages/articole/ascultarea.js
+++ src/pages/articole/ascultarea.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NavBar from "..//../components/NavBar/NavBar"
 import FooterBottom from "..//../components/FooterBottom/FooterBottom"
 import "..//articole/style-edit.css"
+import neascultare from "..//..//..//static/images/articole/neascultare.jpg"
 import profilpoza from "..//..//..//static/images/articole/neascultare.jpg"
 import Fade from "react-reveal/Fade"
 import Helmet from 'react-helmet'
