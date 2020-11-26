11cf81be3818b316b7056c7893d4f2c937c92db9
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-11-26 08:41:38 +0200

    up

--- src/components/Articole/Articole.js
+++ src/components/Articole/Articole.js
@@ -6,7 +6,7 @@ import neascultare from "..//..//..//static/images/articole/neascultare.jpg"
 import oglinda from "..//..//..//static/images/articole/oglinda.jpg"
 import prostia from "..//..//..//static/images/articole/prostia.jpg"
 import stres from "..//..//..//static/images/articole/stres.jpg"
-
+import Link from 'gatsby-link';
 
 
 import Fade from "react-reveal/Fade"
@@ -30,9 +30,13 @@ class ServiciiComponent extends Component {
               <Card.Body>
                 <Card.Title className="title-Card-title">Ascultă-mă ca să te-ascult</Card.Title>
                   <Card.Text>
-                  Te-ai întrebat vreodată dacă ești un bun ascultător? Ce anume face diferența dintre un bun ascultător și un  ascultător jalnic? 
+                    Te-ai întrebat vreodată dacă ești un bun ascultător? Ce anume face diferența dintre un bun ascultător și un  ascultător jalnic? 
                   </Card.Text>
-                <Button variant="dark">Citește mai mult</Button>
+                  <Button variant="dark"><Link 
+                    to="/articole/ascultarea/" 
+                    className="link-articole" 
+                  >  Citește mai mult </Link>
+                  </Button>
               </Card.Body>
             </Card>
             
@@ -43,7 +47,11 @@ class ServiciiComponent extends Component {
                   <Card.Text>
                   Plecând de la binele cunoscut proverb românesc care spune ,,Prostia este omenească" putem înțelege foarte multe lucruri. 
                   </Card.Text>
-                <Button variant="dark">Citește mai mult</Button>
+                  <Button variant="dark"><Link 
+                    to="/articole/prostia/" 
+                    className="link-articole" 
+                  >  Citește mai mult </Link>
+                  </Button>
               </Card.Body>
             </Card>
 
@@ -58,7 +66,11 @@ class ServiciiComponent extends Component {
                   <Card.Text>
                   Viața normală este o viață cu probleme".  Aceasta este fraza cu care mentorul meu obișnuia să înceapă un discurs. 
                   </Card.Text>
-                <Button variant="dark">Citește mai mult</Button>
+                  <Button variant="dark"><Link 
+                    to="/articole/pierderea/" 
+                    className="link-articole" 
+                  >  Citește mai mult </Link>
+                  </Button>
               </Card.Body>
             </Card>
 
@@ -73,7 +85,11 @@ class ServiciiComponent extends Component {
                   <Card.Text>
                   Oamenii nu se trezesc de la o zi la alta, spunând că aspectul lor este inadecvat. Imaginea corporală se dezvoltă gradual.
                   </Card.Text>
-                <Button variant="dark">Citește mai mult</Button>
+                  <Button variant="dark"><Link 
+                    to="/articole/corpul/" 
+                    className="link-articole" 
+                  >  Citește mai mult </Link>
+                  </Button>
               </Card.Body>
             </Card>
 
@@ -84,7 +100,11 @@ class ServiciiComponent extends Component {
                   <Card.Text>
                   Trăim în era vitezei! Dezvoltarea societății prin schimbarea rolurilor pune presiune atât pe femei cât și pe bărbați. 
                   </Card.Text>
-                <Button variant="dark">Citește mai mult</Button>
+                  <Button variant="dark"><Link 
+                    to="/articole/stresul/" 
+                    className="link-articole" 
+                  >  Citește mai mult </Link>
+                  </Button>
               </Card.Body>
             </Card>
           </div>
---
+++ src/pages/articole/ascultarea.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import SeoComponent from "../components/SeoComponent/SeoComponent"
+import NavBar from "../components/NavBar/NavBar"
+
+import FooterBottom from "../components/FooterBottom/FooterBottom"
+
+
+
+class Ascultarea extends Component {
+  render() {
+    return (
+      <div> 
+        <SeoComponent/>
+        <NavBar />
+        <article>
+          
+        </article>
+        <FooterBottom />
+      </div>
+    )
+  }
+}
+export default Ascultarea
