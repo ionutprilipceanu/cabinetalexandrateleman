72cd5bc49e9c27c43ce06e0364b91a6bf9d7c2a4
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-11-27 07:32:18 +0200

    up

---
+++ src/components/SeoComponentArticle/SeoComponentArticle.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import favicon from '..//..//..//static/images/favicon.png'
+import imagineArticol from "..//..//..//static/images/articole/neascultare.jpg" 
+
+
+function SeoComponentArticle() {
+  return (
+        <div>
+            <Helmet>
+            {/* <!-- Primary Meta Tags --> */}
+            <title>Alexandra Teleman - Psihoterapeut | Iași</title>
+            <meta name="title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
+            <meta name="description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <meta name="keywords" content="psiholog iasi, psiholog online, psiholog de familie, psihoterapeut iasi, psiholog, depresie, anxietate, tristețe, dezvoltare personală, psihoterapie, anxietate simptome, terapie de familie, terapie de cuplu, terapie online, terapie individuala"></meta>
+            <meta name="author" content="Alexandra Teleman"/>
+            <meta property="og:locale" content="ro_RO"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
+            <meta property="og:site_name" content="Alexandra Teleman - Psihoterapeut | Iași"></meta>
+            <meta property="og:image" content={imagineArticol}/>
+            <meta property="og:image:width" content="1200"/>
+            <meta property="og:image:height" content="630"/>
+            <meta property="og:image:type" content="image/jpeg" />
+            <link rel="icon" href={favicon} />
+
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://alexandra-teleman.netlify.app/"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
+            <meta property="og:description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
+            {/* <meta property="og:image" content={imagineArticol}/> */}
+	          <meta property="og:image:width" content="1200"/>
+	          <meta property="og:image:height" content="630"></meta>
+
+            {/* <!-- Twitter --> */}
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://alexandra-teleman.netlify.app/"/>
+            <meta property="twitter:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
+            <meta property="twitter:description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
+            {/* <meta property="og:image" content={imagineArticol}/> */}
+          </Helmet>
+        </div>
+  )
+}
+export default SeoComponentArticle
--- src/pages/articole/corpul.js
+++ src/pages/articole/corpul.js
@@ -1,55 +1,15 @@
 import React, { Component } from 'react'
-import SeoComponent from "..//..//components/SeoComponent/SeoComponent"
+// import SeoComponent from "..//..//components/SeoComponent/SeoComponent"
 import NavBar from "..//../components/NavBar/NavBar"
 import FooterBottom from "..//../components/FooterBottom/FooterBottom"
 import "..//articole/style-edit.css"
 import oglinda from "..//..//..//static/images/articole/oglinda.jpg"
 import Fade from "react-reveal/Fade"
-import Helmet from 'react-helmet'
-import favicon from '..//..//..//static/images/favicon.png'
 
 class Corpul extends Component {
   render() {
     return (
       <div> 
-        <Helmet>
-            {/* <!-- Primary Meta Tags --> */}
-            <title>Alexandra Teleman - Psihoterapeut | Iași</title>
-            <meta name="title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
-            <meta name="description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-            <meta name="keywords" content="psiholog iasi, psihoterapeut iasi, psiholog, depresie, anxietate, tristețe, dezvoltare personală, psihoterapie, anxietate simptome, terapie de familie, terapie de cuplu, terapie online, terapie individuala"></meta>
-            <meta name="author" content="Alexandra Teleman"/>
-            <meta property="og:locale" content="ro_RO"/>
-            <meta property="og:type" content="website"/>
-            <meta property="og:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
-            <meta property="og:site_name" content="Alexandra Teleman - Psihoterapeut | Iași"></meta>
-            <meta property="og:image" content={oglinda}/>
-            <meta property="og:image:width" content="1200"/>
-            <meta property="og:image:height" content="630"/>
-            <link rel="icon" href={favicon} />
-            <meta property="og:image" content={oglinda}/>
-	          <meta property="og:image:width" content="1200"/>
-	          <meta property="og:image:height" content="630"></meta> 
-
-
-            {/* <!-- Open Graph / Facebook --> */}
-            <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://alexandra-teleman.netlify.app/"/>
-            <meta property="og:type" content="website"/>
-            <meta property="og:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
-            <meta property="og:description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
-            <meta property="og:image" content={oglinda}/>
-	          <meta property="og:image:width" content="1200"/>
-	          <meta property="og:image:height" content="630"></meta>
-
-            {/* <!-- Twitter --> */}
-            <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content="https://alexandra-teleman.netlify.app/"/>
-            <meta property="twitter:title" content="Alexandra Teleman - Psihoterapeut | Iași"/>
-            <meta property="twitter:description" content="Mă numesc Alexandra și sunt psihoterapeut. Cabinetul este deschis pentru ședințe personalizate, de psihoterapie și coaching, pentru adulți și adolescenți. Investește în tine!"/>
-            <meta property="og:image" content={oglinda}/>
-          </Helmet>
         <NavBar />
         <article>
           <div className="titluArticoleScrise">
