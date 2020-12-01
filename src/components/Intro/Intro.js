53bd48cc4be3847f4ef4cb971be23a2dff339d67
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-12-01 18:33:04 +0200

    up

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -34,10 +34,10 @@ export class Intro extends Component {
                 <div className="aboutSection">
                 <Card.Subtitle className="cardSubtitleSection">Bună, sunt Alexandra. <br/> Îmi pare bine că vrei să mă cunoști! <br/><br/></Card.Subtitle>
                 <Card.Text className="aboutText">
-                Consider că omul este o ființă rațională. El nu v-a acționa în nici un fel dacă consideră că un comportament nu îi v-a aduce beneficiu. Sunt momente când ne simțim copleșiți emoțional și nu mai vedem luminița de la capătul tunelului. Alteori ne putem întreba de ce nu putem fi așa cum visăm să fim! Suntem triști, cu un sentiment puternic de vid. Observăm că relațiile cu ceilalți sunt deficitare și nu înțelegem de ce. 
+                Consider că omul este o ființă rațională. El nu va acționa în nici un fel dacă consideră că un comportament nu îi va aduce beneficiu. Sunt momente când ne simțim copleșiți emoțional și nu mai vedem luminița de la capătul tunelului. Alteori ne putem întreba de ce nu putem fi așa cum visăm să fim! Suntem triști, cu un sentiment puternic de vid. Observăm că relațiile cu ceilalți sunt deficitare și nu înțelegem de ce. 
                 <br/><br/>
                 Rolul psihologului este tocmai acela de a te ajuta să înțelegi și să controlezi toate acestea, urcând împreună, același deal, indiferent de plouă sau ninge!
-                Noi știm că după toate v-a ieși și soarele!
+                Noi știm că după toate va ieși și soarele!
                 Conștientizarea este primul pas care te face să acționezi! <br/>
                 Investește-n tine!
 
